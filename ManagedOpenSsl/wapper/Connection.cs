f7993a27fe8776e18140c6c542d75bbf8b42431f
edomurasaki2000 <edomurasaki2000@cock.li>
2018-08-18 20:20:48 +0900

    first commit

---
+++ ManagedOpenSsl/wapper/Connection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+namespace OpenSSL.wapper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SslConnection : IDisposable
+    {
+
+        TcpClient tcp = null;
+        OpenSSL.SSL.SslStream ssl = null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        public void connect(Uri url)
+        {
+            tcp = new TcpClient();
+
+            tcp.Connect(url.Host, 443);
+
+            ssl = new OpenSSL.SSL.SslStream(tcp.GetStream());
+            ssl.AuthenticateAsClient(url.Host);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="offset"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public int read(byte[] buffer, int offset, int length)
+        {
+            return ssl.Read(buffer, offset, length);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fmt"></param>
+        /// <param name="args"></param>
+        public void write(string fmt, params object[] args)
+        {
+            write(string.Format(fmt, args));
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="line"></param>
+        public void write(string line)
+        {
+            var bytes = Encoding.UTF8.GetBytes(line);
+            ssl.Write(bytes, 0, bytes.Length);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string getLine()
+        {
+            bool got15 = false;
+            var sb = new StringBuilder();
+            while (true)
+            {
+                int i = ssl.ReadByte();
+                sb.Append((char)i);
+                if (i == '\r') got15 = true;
+                else if (i == '\n' && got15)
+                    break;
+                else
+                    got15 = false;
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            ssl.Dispose();
+            tcp.Close();
+        }
+    }
+
+}
---
+++ ManagedOpenSsl/wapper/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+namespace OpenSSL.wapper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Extensions
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public static string getContentType(this WebHeaderCollection col)
+        {
+
+            return col[HttpResponseHeader.ContentType];
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        public static long getContentLength(this WebHeaderCollection col)
+        {
+            long l = -1;
+            if (col[HttpResponseHeader.ContentLength] != null)
+                l = long.Parse(col[HttpResponseHeader.ContentLength]);
+            return l;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="schema"></param>
+        /// <param name="value"></param>
+        public static void setAuthorization(this WebHeaderCollection col, string schema, string value)
+        {
+            col.Add(HttpRequestHeader.Authorization,
+                string.Format("{0} {1}", schema, value));
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        public static void setBasicAuthorization(this WebHeaderCollection col, string username, string password)
+        {
+            setAuthorization(col, "Basic",
+                Convert.ToBase64String(Encoding.UTF8.GetBytes(username + ":" + password))
+                );
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ms"></param>
+        /// <param name="enc"></param>
+        /// <returns></returns>
+        public static string getString(this System.IO.MemoryStream ms, System.Text.Encoding enc = null)
+        {
+            enc = enc == null ? System.Text.Encoding.UTF8 : enc;
+            return enc.GetString(ms.ToArray());
+        }
+    }
+   
+}
